6cb49cc7f29aa9474a07a475c411f5b3cb85004f
Matheus <matheus.barboza.henrique@gmail.com>
2020-06-17 16:11:52 -0300

    inicio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AtividadeFixacaoIngresso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
